774b8479da5b36980921fbaa0be41c46f12fcb4a
eva.Kryst <eva.kryst@hotmail.com>
2023-12-11 19:47:48 +0200

    Main updated

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.proj5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello CF!");
+        System.out.print("Hello Coding Factory!.!.!.!.");
     }
 }
