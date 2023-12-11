6f6c7f95dc869fe90099f75e149c80499c69f52d
eva.Kryst <eva.kryst@hotmail.com>
2023-12-11 20:28:11 +0200

    Main updated II

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello Coding Factory!.!.!.!.");
     }
+    public static void sayHello() {
+        System.out.println("Hello world!!!");
+    }
 }
